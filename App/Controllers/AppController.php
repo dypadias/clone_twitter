064f6d37955ba84a26f564a148c8c49e710e5cfb
Diego Matos <dypadias@gmail.com>
2022-05-21 05:06:56 -0300

    Inicio Projeto Clone Twiteer

---
+++ App/Controllers/AppController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Controllers;
+
+//os recursos do miniframework
+use MF\Controller\Action;
+use MF\Model\Container;
+
+class AppController extends Action
+{
+    public function timeline()
+    {
+        session_start();
+
+        if ($_SESSION['id'] != '' && $_SESSION['nome'] != '') {
+
+            //recuperar tweets
+            $tweet = Container::getModel('Tweet');
+            $tweet->__set('id_usuario', $_SESSION['id']);
+
+            $tweets = $tweet->getAll();
+
+            $this->view->tweets = $tweets;
+
+            $this->render('timeline');
+        } else {
+            header("Location: /?login=erro");
+        }
+    }
+
+    public function tweet()
+    {
+        session_start();
+
+        if ($_SESSION['id'] != '' && $_SESSION['nome'] != '') {
+
+            $tweet = Container::getModel('Tweet');
+            $tweet->__set('tweet', $_POST['tweet']);
+            $tweet->__set('id_usuario', $_SESSION['id']);
+
+            $tweet->salvar();
+            header("Location: /timeline");
+        } else {
+            header("Location: /?login=erro");
+        }
+    }
+}
